e23dfa03de5ed3128083ec2fdcc4ffacde1e2f48
Enrico Torres <enricoftorres0101@gmail.com>
2025-11-17 13:22:51 -0300

    started neck movement

--- joy2twist/scripts/joy_speech.py
+++ joy2twist/scripts/joy_speech.py
@@ -2,12 +2,13 @@
 
 import rclpy
 from rclpy.node import Node
-
-from sensor_msgs.msg import Joy
+from sensor_msgs.msg import Joy, JointState
 from rclpy.qos import qos_profile_sensor_data
 from fbot_speech_msgs.srv import SynthesizeSpeech
+from std_msgs.msg import Bool, Float64MultiArray
 
 BUTTON_Y = 3
+BUTTON_X = 2
 DPAD_VERTICAL = 5
 DPAD_HORIZONTAL = 4
 
@@ -25,36 +26,72 @@ class JoystickSpeechNode(Node):
     def initRosComm(self):
         self.joy_sub_ = self.create_subscription(Joy, 'joy', self.joy_cb, qos_profile_sensor_data)
         self.cli = self.create_client(SynthesizeSpeech, "/fbot_speech/ss/say_something")
+        self.neck_pub_ = self.create_publisher(Float64MultiArray, "/updateNeck", 1)
+        self.joint_sub_ = self.create_subscription(JointState, "/boris_head/joint_states", self.joint_cb)
 
+    def joint_cb(self, msg: JointState):
+        self.joint = msg
+        self.joint.position = msg.position
+        self.get_logger().info(str(self.joint.position))
 
+        
     def joy_cb(self, joy_msg):
 
         self.current_DPAD_vertical_state = joy_msg.axes[DPAD_VERTICAL]
         self.current_DPAD_horizontal_state = joy_msg.axes[DPAD_HORIZONTAL]
-        self.current_BUTTON_Y_state = joy_msg.buttons[3]
+        self.current_BUTTON_Y_state = joy_msg.buttons[BUTTON_Y]
+        self.current_BUTTON_X_state = joy_msg.buttons[BUTTON_X]
 
         if self.current_BUTTON_Y_state == 1:
-
+            
+            #cima
             if self.current_DPAD_vertical_state == 1 and self.last_DPAD_vertical_state == 0:
                 self.get_logger().info("Botão pressionado")
                 self.create_message('Hello my name is Boris', 'en')
-
+            
+            #baixo
             elif self.current_DPAD_vertical_state == -1 and self.last_DPAD_vertical_state == 0:
                 self.get_logger().info("Botão pressionado")
                 self.create_message('The FBOT is the Robotics Group at FURG (Federal University of Rio Grande) focused on developing projects in autonomous mobile robotics. ' \
                                 'Their main objective is to prepare students for national and international competitions, applying knowledge in electronics, programming, and artificial intelligence.', 'en')
             
+            #direita
             elif self.current_DPAD_horizontal_state == 1 and self.last_DPAD_horizontal_state == 0:
                 self.get_logger().info("Botão pressionado")
                 self.create_message('I come from FURG, the Federal University of Rio Grande, a public higher education institution recognized for its excellence in teaching, research, and outreach (extension).', 'en')
-
+            
+            #esquerda
             elif self.current_DPAD_horizontal_state == -1 and self.last_DPAD_horizontal_state == 0:
                 self.get_logger().info("Botão pressionado")
                 self.create_message('Currently, I am a four-time brazilian robotics competition champion. ', 'en')
 
+
+        elif self.current_BUTTON_X_state == 1:
+           
+           #cima
+            if self.current_DPAD_vertical_state == 1 and self.last_DPAD_vertical_state == 0:
+                self.get_logger().info("Botão para cima pressionado")
+                
+            #baixo
+            elif self.current_DPAD_vertical_state == -1 and self.last_DPAD_vertical_state == 0:
+                self.get_logger().info("Botão para baixo pressionado")
+                
+            #direita
+            elif self.current_DPAD_horizontal_state == 1 and self.last_DPAD_horizontal_state == 0:
+                self.get_logger().info("Botão para direita pressionado")
+                
+            #esquerda
+            elif self.current_DPAD_horizontal_state == -1 and self.last_DPAD_horizontal_state == 0:
+                self.get_logger().info("Botão para esquerda pressionado")
+                
+
+
         self.last_DPAD_horizontal_state = self.current_DPAD_horizontal_state
         self.last_DPAD_vertical_state = self.current_DPAD_vertical_state
 
+    def neck_movement(self, x_angle, z_angle):
+        neck = Float64MultiArray.Request()
+        neck.data = [x_angle, z_angle]
 
     def create_message(self, text, lang):
         self.get_logger().info('Creating message: "%s" in %s' % (text, lang))
