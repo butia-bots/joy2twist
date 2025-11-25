b2c03e93d94b1a01aa409b3b6cdcbbc77b187334
Enrico Torres <enricoftorres0101@gmail.com>
2025-11-25 12:12:49 -0300

    launch built, test

--- joy2twist/launch/speech_head_controller.launch.py
+++ joy2twist/launch/speech_head_controller.launch.py
@@ -1,8 +1,10 @@
 from launch_ros.actions import Node
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import EnvironmentVariable, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
     namespace = LaunchConfiguration("namespace")
@@ -28,10 +30,28 @@ def generate_launch_description():
         remappings=[("/diagnostics", "diagnostics")],
     )
 
+    neck = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory("fbot_hri_bringup"), 'neck.launch.py')
+        ),
+    )
+
+    synthesizer_speech = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory("fbot_hri_bringup"), 'synthesizer_speech.launch.py')
+        
+        ),
+        launch_arguments={
+            'use_remote': LaunchConfiguration("use_remote"),
+        }.items()
+    )
+
     actions = [
         declare_namespace_arg,
         joy_speech,
-        joy
+        joy,
+        neck,
+        synthesizer_speech
 
     ]
 
